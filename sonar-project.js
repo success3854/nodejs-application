0843845695b236a30947dd439ceb0fdcf787a4e8
success3854 <105676617+success3854@users.noreply.github.com>
2022-07-04 21:59:24 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.139.55.35:9000/',
+  serverUrl: 'http://3.143.227.133:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
